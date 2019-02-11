92cf4c3fcbea5693e1a42fc69592018ee927af43
Simon <01secn@gmail.com>
2019-02-11 15:07:30 +0000

    Refactored tests to have shared set up - creating oystercard instance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,18 +3,18 @@
 require 'oystercard'
 
 describe Oystercard do
-  it 'should set balance on new oyster cards to 0' do
-    oystercard = Oystercard.new
+  before(:each) do
+    @oystercard = Oystercard.new
+  end
 
-    expect(oystercard.balance).to eq(0)
+  it 'should set balance on new oyster cards to 0' do
+    expect(@oystercard.balance).to eq(0)
   end
 
   it 'should increase the balance by the top up amount' do
-    oystercard = Oystercard.new
-    initial_balance = oystercard.balance
+    initial_balance = @oystercard.balance
+    @oystercard.top_up(10)
 
-    oystercard.top_up(10)
-
-    expect(oystercard.balance - initial_balance).to eq(10)
+    expect(@oystercard.balance - initial_balance).to eq(10)
   end
 end
