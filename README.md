9c7bc15dc6ffc9f01093b864d5eded5fd3d0f3c2
Simon <01secn@gmail.com>
2019-02-11 14:53:48 +0000

    Added default balance of 0 for new oyster cards

---
+++ README.md
@@ -0,0 +1,6 @@
+# How to run in irb
+
+Run irb
+require './lib/oystercard.rb'
+
+oystercard = Oystercard.new
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,9 @@
-require "oystercard"
-describe Oystercard do
+require 'oystercard'
 
+describe Oystercard do
+  it 'should set balance on new oyster cards to 0' do
+    oystercard = Oystercard.new
+
+    expect(oystercard.balance).to eq(0)
+  end
 end
