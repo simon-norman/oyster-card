1a3716d1f0ec71c540257f970aa72ef23fc019a3
Simon <01secn@gmail.com>
2019-02-11 15:02:24 +0000

    Added top up method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,4 +5,8 @@ class Oystercard
   def initialize
     @balance = DEFAULT_BALANCE
   end
+
+  def top_up(amount)
+    @balance += amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'oystercard'
 
 describe Oystercard do
@@ -5,5 +7,14 @@ describe Oystercard do
     oystercard = Oystercard.new
 
     expect(oystercard.balance).to eq(0)
+  end
+
+  it 'should increase the balance by the top up amount' do
+    oystercard = Oystercard.new
+    initial_balance = oystercard.balance
+
+    oystercard.top_up(10)
+
+    expect(oystercard.balance - initial_balance).to eq(10)
   end
 end
